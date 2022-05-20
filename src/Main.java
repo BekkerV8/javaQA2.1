396f3fe3d3de332ce798d95325bca3329561c419
BekkerV8 <valkenshtein@gmail.com>
2022-05-20 10:40:07 -0700

    git add

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 111_234;
+        int x = 20;
+
+        int bonus = amount / x;
+
+        System.out.println("Начислено миль " + bonus);
+    }
+}
